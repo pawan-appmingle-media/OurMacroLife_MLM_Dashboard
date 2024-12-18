cccae2fdb73eb1280fd76d84b5bef5e0933c1198
Pawan Kumar <pawan.kumar@appminglemedia.com>
2024-12-18 12:04:03 +0530

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,13 +6,9 @@ import image from "../images/loginImage.png";
 const Login = () => {
   const navigate = useNavigate();
   const [mobileNumber, setMobileNumber] = useState("");
-  // login=true
-  // token = 4ZycSPmxbyZVFgjjBhO6kNmwA76fINLvrHShTWUbTVLuJb1dokm4IxzbdUOg
-  // userId = 34;
   const [otpSent, setOtpSent] = useState(false);
   const [otp, setOtp] = useState("");
   const baseURL = process.env.REACT_APP_API_BASE_URL;
-  // console.log("Auth Header:", axios.defaults.headers.common["Authorization"]);
 
   const fetchLogin = async (mobileNumber) => {
     try {
@@ -26,11 +22,6 @@ const Login = () => {
         }
       );
       console.log("MSG & OTP", response.data);
-
-      // const { vendor } = response.data;
-      // console.log("vendor =>", vendor);
-      // localStorage.setItem("userId", vendor.id);
-      // return vendor.api_token;
     } catch (error) {
       console.error("Error in login: ", error.response?.data || error.message);
       alert(
@@ -45,15 +36,7 @@ const Login = () => {
     if (mobileNumber && mobileNumber.length === 10) {
       const token = await fetchLogin(mobileNumber);
       setOtpSent(true);
-
-      // localStorage.setItem("token", token);
-      // console.log("the token is -> ", token);
-
       axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-      // console.log(
-      //   "Auth Header =>",
-      //   axios.defaults.headers.common["Authorization"]
-      // );
     } else {
       alert("Please enter a valid 10-digit mobile number");
     }
@@ -62,25 +45,27 @@ const Login = () => {
   const handleLogin = async () => {
     if (otp) {
       try {
-        const response = await axios.post(
-          `${baseURL}/vendor/verify-otp`,
-          {
-            phone_number: mobileNumber,
-            otp: otp,
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          }
-        );
-        console.log("OTP Verification Response:->", response.data);
+        // const response = await axios.post(
+        //   `${baseURL}/vendor/verify-otp`,
+        //   {
+        //     phone_number: mobileNumber,
+        //     otp: otp,
+        //   },
+        //   {
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //       Authorization: `Bearer ${localStorage.getItem("token")}`,
+        //     },
+        //   }
+        // );
+        // console.log("OTP Verification Response:->", response.data);
         alert("OTP Verified! Logging in...");
 
         // Store login status
         localStorage.setItem("login", true); // This flag can be used to check login status
-        const token = response.data.token;
+        // const token = response.data.token;
+        const token =
+          "FTVL8BYSRrCtzA6gY0j4TcgFLdlaFD7VxFWb2HQmssKfOCKUXFxImWiajs4k";
         localStorage.setItem("token", token);
 
         navigate("/dashboard");
@@ -129,20 +114,7 @@ const Login = () => {
               className="w-full p-2 border border-gray-300 rounded-lg"
             />
           </div>
-          {/* <div className="mb-4">
-            <label htmlFor="mobile_number" className="block mb-2 text-gray-600">
-              Device Id
-            </label>
-            <input
-              type="text"
-              id="mobile"
-              name="device_id"
-              value={deviceId}
-              onChange={(e) => setdeviceId(e.target.value)}
-              placeholder="Enter your mobile number"
-              className="w-full p-2 border border-gray-300 rounded-lg"
-            />
-          </div> */}
+
           {/* Send OTP Button */}
           {!otpSent && (
             <button
